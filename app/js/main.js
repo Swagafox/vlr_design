de1702a4ad821587bda08ad7bdb66d73a8adc952
Swagafox <igory.udenko@gmail.com>
2022-09-21 21:25:06 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".header-nav__link").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 800);
+  });
+});
