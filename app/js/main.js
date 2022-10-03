cd65b8fad8888a74f287cf6b6ff1632ec9cc8fcc
Swagafox <81470088+Swagafox@users.noreply.github.com>
2022-10-03 20:20:16 +0300

    Delete app directory

--- app/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $(".header-nav__link, .button__top").on("click", function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-      top = $(id).offset().top;
-    $('body,html').animate({ scrollTop: top }, 800);
-  });
-});
